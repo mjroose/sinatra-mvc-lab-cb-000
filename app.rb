79e448c2b1aae4cd262cf9c9985297230c6407c9
Michael Roose <mjroose@gmail.com>
2018-10-27 15:50:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+
+    erb :result
+  end
 end
