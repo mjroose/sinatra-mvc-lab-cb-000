e317dece2b6d89bebc31cd60b9a3e6e4ac61e662
Michael Roose <mjroose@gmail.com>
2018-10-27 16:02:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,13 @@
 class PigLatinizer
   def initialize(user_phrase)
-    @text = user_input
+    @words = user_input.split(" ")
   end
 
   def piglatinize
     @text
+  end
+
+  def starts_with_vowel?(word)
+    !!word.match(/\A[aeiouAEIOU]/)
   end
 end
